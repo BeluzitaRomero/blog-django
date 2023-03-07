2519c3618c80b3205827668fd7774d484ded2293
BeluzitaRomero <romero.abelen@gmail.com>
2023-03-06 22:49:42 -0300

    Post + created_at and Comment model

--- PostsApp/models.py
+++ PostsApp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.contrib.auth.models import User
+from datetime import timezone
 
 # Create your models here.
 class Post(models.Model):
@@ -10,6 +11,14 @@ class Post(models.Model):
     subtitle = models.CharField(max_length=250, null=True, blank=True, default='')
     post_description = RichTextUploadingField(null=True, blank=True)
     post_img = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+    
+
+class Comment(models.Model):
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=140)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
--- PostsApp/views.py
+++ PostsApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
-from django.contrib.auth.models import User
+
 
 
 def all_posts(request):
